5b0b3200bc0f5a9c79d18b08845ab100e96d73f2
jacobsweany <30737348+jacobsweany@users.noreply.github.com>
2017-08-14 20:28:05 -0700

    Update ConvertWinEvt.ps1

--- ConvertWinEvt.ps1
+++ ConvertWinEvt.ps1
@@ -1,11 +1,19 @@
+
+# Import AD session
 $session = New-PSSession -ComputerName LON-DC1
 Invoke-Command -Session $session -ScriptBlock { Import-Module activedirectory }
 Import-PSSession $session
+
 $LocalComputer = $env:COMPUTERNAME
+
 $TargetOU = 'ou=Servers,dc=adatum,dc=com'
 $events = Get-WinEvent -ComputerName $LocalComputer -FilterHashtable @{ Logname = "Application"; ID = 41015 }
+
+# Loop through each event within specified parameters
 foreach ($event in $events) {
+  # Convert to XML
   $eventXML = [xml]$event.ToXML()
+    # Convert to data readable by PowerShell
     $eventData = $eventXML.Event.EventData.Data
     $eventData | % {
         if ($_ -match "computer (.+).") {
